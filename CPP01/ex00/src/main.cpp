7b91dddcb3a416ea0ba465ce4aa0cd4f563f9d3e
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-03-11 16:49:31 +0000

    Set up basic Zombie class.

--- CPP01/ex00/src/main.cpp
+++ CPP01/ex00/src/main.cpp
@@ -1,6 +1,11 @@
 #include "../inc/data.h"
+#include "../inc/Zombie.hpp"
 
-int	main(int ac, char **av)
+int	main() //int ac, char **av
 {
+	Zombie	steve("Steve");
+
+
+	std::cout << steve.getName() << "\n";
 	return (0);
 }
